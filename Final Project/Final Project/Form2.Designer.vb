cb16faec002c5262a6002365976dc86fcd3b4e5e
leonbekker <45794158+leonbekker@users.noreply.github.com>
2018-12-11 21:07:12 +0200

    Add files via upload

---
+++ Final Project/Final Project/Form2.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.black_blok1 = New System.Windows.Forms.PictureBox
+        Me.black_blok2 = New System.Windows.Forms.PictureBox
+        Me.black_blok3 = New System.Windows.Forms.PictureBox
+        Me.black_blok4 = New System.Windows.Forms.PictureBox
+        Me.black_blok5 = New System.Windows.Forms.PictureBox
+        Me.black_blok6 = New System.Windows.Forms.PictureBox
+        Me.black_blok7 = New System.Windows.Forms.PictureBox
+        Me.black_blok8 = New System.Windows.Forms.PictureBox
+        Me.ra = New System.Windows.Forms.PictureBox
+        Me.tmr_rara = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        CType(Me.black_blok1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.black_blok2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.black_blok3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.black_blok4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.black_blok5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.black_blok6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.black_blok7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.black_blok8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ra, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'black_blok1
+        '
+        Me.black_blok1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok1.Location = New System.Drawing.Point(-2, 371)
+        Me.black_blok1.Name = "black_blok1"
+        Me.black_blok1.Size = New System.Drawing.Size(116, 200)
+        Me.black_blok1.TabIndex = 0
+        Me.black_blok1.TabStop = False
+        '
+        'black_blok2
+        '
+        Me.black_blok2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok2.Location = New System.Drawing.Point(109, 358)
+        Me.black_blok2.Name = "black_blok2"
+        Me.black_blok2.Size = New System.Drawing.Size(116, 213)
+        Me.black_blok2.TabIndex = 1
+        Me.black_blok2.TabStop = False
+        '
+        'black_blok3
+        '
+        Me.black_blok3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok3.Location = New System.Drawing.Point(221, 371)
+        Me.black_blok3.Name = "black_blok3"
+        Me.black_blok3.Size = New System.Drawing.Size(116, 200)
+        Me.black_blok3.TabIndex = 2
+        Me.black_blok3.TabStop = False
+        '
+        'black_blok4
+        '
+        Me.black_blok4.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok4.Location = New System.Drawing.Point(329, 340)
+        Me.black_blok4.Name = "black_blok4"
+        Me.black_blok4.Size = New System.Drawing.Size(116, 231)
+        Me.black_blok4.TabIndex = 3
+        Me.black_blok4.TabStop = False
+        '
+        'black_blok5
+        '
+        Me.black_blok5.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok5.Location = New System.Drawing.Point(436, 350)
+        Me.black_blok5.Name = "black_blok5"
+        Me.black_blok5.Size = New System.Drawing.Size(127, 221)
+        Me.black_blok5.TabIndex = 4
+        Me.black_blok5.TabStop = False
+        '
+        'black_blok6
+        '
+        Me.black_blok6.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok6.Location = New System.Drawing.Point(555, 371)
+        Me.black_blok6.Name = "black_blok6"
+        Me.black_blok6.Size = New System.Drawing.Size(155, 200)
+        Me.black_blok6.TabIndex = 5
+        Me.black_blok6.TabStop = False
+        '
+        'black_blok7
+        '
+        Me.black_blok7.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok7.Location = New System.Drawing.Point(706, 358)
+        Me.black_blok7.Name = "black_blok7"
+        Me.black_blok7.Size = New System.Drawing.Size(155, 213)
+        Me.black_blok7.TabIndex = 6
+        Me.black_blok7.TabStop = False
+        '
+        'black_blok8
+        '
+        Me.black_blok8.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.black_blok8.Location = New System.Drawing.Point(855, 184)
+        Me.black_blok8.Name = "black_blok8"
+        Me.black_blok8.Size = New System.Drawing.Size(57, 387)
+        Me.black_blok8.TabIndex = 7
+        Me.black_blok8.TabStop = False
+        '
+        'ra
+        '
+        Me.ra.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ra.Location = New System.Drawing.Point(747, 251)
+        Me.ra.Name = "ra"
+        Me.ra.Size = New System.Drawing.Size(55, 107)
+        Me.ra.TabIndex = 8
+        Me.ra.TabStop = False
+        '
+        'tmr_rara
+        '
+        Me.tmr_rara.Enabled = True
+        Me.tmr_rara.Interval = 10
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ProgressBar1.Location = New System.Drawing.Point(2, 2)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(248, 39)
+        Me.ProgressBar1.TabIndex = 9
+        Me.ProgressBar1.Value = 100
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(887, 565)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ra)
+        Me.Controls.Add(Me.black_blok8)
+        Me.Controls.Add(Me.black_blok7)
+        Me.Controls.Add(Me.black_blok6)
+        Me.Controls.Add(Me.black_blok5)
+        Me.Controls.Add(Me.black_blok4)
+        Me.Controls.Add(Me.black_blok3)
+        Me.Controls.Add(Me.black_blok2)
+        Me.Controls.Add(Me.black_blok1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.black_blok1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.black_blok2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.black_blok3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.black_blok4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.black_blok5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.black_blok6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.black_blok7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.black_blok8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ra, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents black_blok1 As System.Windows.Forms.PictureBox
+    Friend WithEvents black_blok2 As System.Windows.Forms.PictureBox
+    Friend WithEvents black_blok3 As System.Windows.Forms.PictureBox
+    Friend WithEvents black_blok4 As System.Windows.Forms.PictureBox
+    Friend WithEvents black_blok5 As System.Windows.Forms.PictureBox
+    Friend WithEvents black_blok6 As System.Windows.Forms.PictureBox
+    Friend WithEvents black_blok7 As System.Windows.Forms.PictureBox
+    Friend WithEvents black_blok8 As System.Windows.Forms.PictureBox
+    Friend WithEvents ra As System.Windows.Forms.PictureBox
+    Friend WithEvents tmr_rara As System.Windows.Forms.Timer
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+End Class
---
+++ Final Project/Final Project/Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+    Private Sub rara_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmr_rara.Tick
+        ra.Left -= 5
+        For Each black_bloks In Me.Controls
+            If black_bloks.name.ToString.Contains("black_blok") Then
+                If ra.Bounds.IntersectsWith(black_bloks.bounds) Then
+                    If ra.Bottom > black_bloks.top Then
+                    End If
+                End If
+            End If
+        Next
+    End Sub
+End Class
